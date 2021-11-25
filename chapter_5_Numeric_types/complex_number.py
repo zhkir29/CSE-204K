6a06289f9e946a2042041b891ddd041490d140d8
zhkir29 <zh2018131041@gmail.com>
2021-11-25 20:31:14 +0600

    a

---
+++ chapter_5_Numeric_types/complex_number.py
@@ -0,0 +1,3 @@
+#The complex number with a real part of 2 and an imaginary part of -3 is written 2+(-3j)
+#Complex also allow us to extract their parts as attributes ,support all the usual mathematical expressions, and may be processed with tools in 
+# the standard 'cmath' module (the complex version of the standard math module.)
---
+++ chapter_5_Numeric_types/integer_precision.py
@@ -0,0 +1,7 @@
+#Python 2.X has a separate type for long integers , but it autometically converts any number to large to store in a normal integer to this type.
+# Hence ,you don't need to code any special syntax to use longs ,and the only way you can tell that you are using 2.0X longs in that they print 
+# with a trailing 'L'.Python 3.X supports unlimited size of integers.
+
+print(99999999999999999999999999999999999999999999+1)
+
+#But in python 2 resutl will be  10000000000000000000000000000000000000000000000000L
