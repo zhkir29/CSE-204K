eb8c3a7d9ccf213880b347bbc807905048b1d192
zhkir29 <zh2018131041@gmail.com>
2021-11-25 19:45:29 +0600

    a

---
+++ chapter_5_Numeric_types/floor_vs_truction.py
@@ -0,0 +1,22 @@
+import math
+print(math.floor(2.5))
+print(math.floor(-2.5))
+print(math.floor(2.5))
+print(math.trunc(-2.5))
+print('------------Python 3.0X-------------')
+x=(5/2,-5/2)
+print(x)
+
+y=(5//2,5//-2)    #floor is the same as trunciton for positives.
+print(y)
+
+print((5/2.0,5/-2.0))
+
+print((5//2.0,5//-2.0))
+
+print("--------------------Python 2.0X -------------------")
+print((5/2,5/-2)) #That differs in python 3.0X from python 2.0X and all are same
+print()
+print(int(10/3))  #same as "trunc" function because it cut off after the decimal point like trunc function.
+
+
